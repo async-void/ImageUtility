a2f39ccfdf56faa53f74121562c3f43478ae1cea
g-larose <larose.g@fromtier.com>
2025-10-04 04:00:10 -0400

    Renamer View work

--- ImageUtility/Features/Renamer/RenamerViewModel.cs
+++ ImageUtility/Features/Renamer/RenamerViewModel.cs
@@ -9,16 +9,18 @@ namespace ImageUtility.Features.Renamer
     public partial class RenamerViewModel : ViewModelBase
     {
         [ObservableProperty]
+        [Required]
         [NotifyCanExecuteChangedFor(nameof(RenameCommand))]
         private string _sourceDir;
 
         [ObservableProperty]
+        [Required]
         [NotifyCanExecuteChangedFor(nameof(RenameCommand))]
         private string _destinationDir;
 
         [ObservableProperty]
         [NotifyCanExecuteChangedFor(nameof(RenameCommand))]
-        private string _pattern;
+        private string? _pattern;
 
         public RenamerViewModel() : base("Renamer", MaterialIconKind.Rename, 2)
         {
@@ -28,6 +30,21 @@ namespace ImageUtility.Features.Renamer
         private void Rename()
         {
 
+        }
+
+        [RelayCommand]
+        private void SetSourceDirectory()
+        {
+        }
+
+        [RelayCommand]
+        private void SetDestinationDirectory()
+        {
+        }
+
+        [RelayCommand]
+        private void LoadExternal()
+        {
         }
 
         public bool CanRename() => !string.IsNullOrEmpty(Pattern) && !string.IsNullOrEmpty(SourceDir) && !string.IsNullOrEmpty(DestinationDir);
