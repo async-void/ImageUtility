00f240121ab0de12b74b9e08d453024f92933afe
g-larose <larose.g@fromtier.com>
2025-11-21 03:02:54 -0500

    working on UI bug fixes

---
+++ ImageUtility/Features/Feedback/FeedBackView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ImageUtility.Features.Feedback;
+
+public partial class FeedBackView : UserControl
+{
+    public FeedBackView()
+    {
+        InitializeComponent();
+    }
+}
